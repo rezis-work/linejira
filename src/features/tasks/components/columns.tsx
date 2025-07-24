1518819eec57e4ba5d7bf1cfdd129cc589286aa0
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-07-24 15:58:28 +0400

    implement tasks update and delete

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, MoreVertical } from "lucide-react";
 
 import { ColumnDef } from "@tanstack/react-table";
 import { Task } from "../types";
@@ -8,6 +8,8 @@ import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { MemberAvatar } from "@/features/members/components/member-avatar";
 import { TaskDate } from "./task-date";
 import { Badge } from "@/components/ui/badge";
+import { snakeCaseToTitleCase } from "@/lib/utils";
+import { TaskActions } from "./task-actions";
 
 export const columns: ColumnDef<Task>[] = [
   {
@@ -120,7 +122,22 @@ export const columns: ColumnDef<Task>[] = [
     cell: ({ row }) => {
       const status = row.original.status;
 
-      return <Badge variant="outline">{status}</Badge>;
+      return <Badge variant={status}>{snakeCaseToTitleCase(status)}</Badge>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const id = row.original.$id;
+      const projectId = row.original.projectId;
+
+      return (
+        <TaskActions id={id} projectId={projectId}>
+          <Button className="size-8 p-0" variant={"ghost"}>
+            <MoreVertical className="size-4" />
+          </Button>
+        </TaskActions>
+      );
     },
   },
 ];
---
+++ src/features/tasks/components/task-actions.tsx
@@ -0,0 +1,90 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ExternalLinkIcon, PencilIcon, TrashIcon } from "lucide-react";
+import { useDeleteTask } from "../api/use-delete-task";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useEditTaskModal } from "../hooks/use-edit-task-modal";
+
+interface TaskActionsProps {
+  id: string;
+  projectId: string;
+  children: React.ReactNode;
+}
+
+export function TaskActions({ id, projectId, children }: TaskActionsProps) {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
+  const { open } = useEditTaskModal();
+  const { mutate: deleteTask, isPending } = useDeleteTask();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Delete Task",
+    "This action cannot be undone.",
+    "destructive"
+  );
+
+  const handleDeleteTask = async () => {
+    const ok = await confirm();
+    if (!ok) return;
+
+    deleteTask({ param: { taskId: id } });
+  };
+
+  const onOpenTask = () => {
+    router.push(`/workspaces/${workspaceId}/tasks/${id}`);
+  };
+
+  const onOpenProject = () => {
+    router.push(`/workspaces/${workspaceId}/projects/${projectId}`);
+  };
+
+  return (
+    <div className="flex justify-end">
+      <ConfirmDialog />
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuItem
+            onClick={onOpenTask}
+            disabled={isPending}
+            className="font-medium p-[10px]"
+          >
+            <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
+            Task Details
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={onOpenProject}
+            disabled={isPending}
+            className="font-medium p-[10px]"
+          >
+            <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
+            Open Project
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => {
+              open(id);
+            }}
+            disabled={isPending}
+            className="font-medium p-[10px]"
+          >
+            <PencilIcon className="size-4 mr-2 stroke-2" />
+            Edit Task
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={handleDeleteTask}
+            disabled={isPending}
+            className="font-medium text-amber-700 focus:text-amber-700 p-[10px]"
+          >
+            <TrashIcon className="size-4 mr-2 stroke-2" />
+            Delete Task
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
